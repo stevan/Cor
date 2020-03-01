af8542fa1180152423c40c10da730408d5b9273b
Stevan Little <stevan.little@gmail.com>
2020-03-02 00:15:25 +0100

    simplify the location stuff a bit (I think)

--- lib/Cor/Builder.pm
+++ lib/Cor/Builder.pm
@@ -13,11 +13,11 @@ use Cor::Builder::Method;
 
 use Cor::Builder::Location;
 
-sub new_role      {      Cor::Builder::Role->new( location => Cor::Builder::Location->new( @_ ) ) }
-sub new_class     {     Cor::Builder::Class->new( location => Cor::Builder::Location->new( @_ ) ) }
-sub new_reference { Cor::Builder::Reference->new( location => Cor::Builder::Location->new( @_ ) ) }
-sub new_slot      {      Cor::Builder::Slot->new( location => Cor::Builder::Location->new( @_ ) ) }
-sub new_method    {    Cor::Builder::Method->new( location => Cor::Builder::Location->new( @_ ) ) }
+sub new_role_at      {      Cor::Builder::Role->new( location => Cor::Builder::Location->new( start => $_[0] ) ) }
+sub new_class_at     {     Cor::Builder::Class->new( location => Cor::Builder::Location->new( start => $_[0] ) ) }
+sub new_reference_at { Cor::Builder::Reference->new( location => Cor::Builder::Location->new( start => $_[0] ) ) }
+sub new_slot_at      {      Cor::Builder::Slot->new( location => Cor::Builder::Location->new( start => $_[0] ) ) }
+sub new_method_at    {    Cor::Builder::Method->new( location => Cor::Builder::Location->new( start => $_[0] ) ) }
 
 1;
 
--- lib/Cor/Builder/Location.pm
+++ lib/Cor/Builder/Location.pm
@@ -8,14 +8,10 @@ use decorators   qw[ :accessors ];
 use parent 'UNIVERSAL::Object::Immutable';
 
 use slots (
-    pos    => sub { die 'The `pos` is required'    },
-    length => sub { die 'The `length` is required' },
+    start => sub { die 'The `start` postion is required' },
 );
 
-sub pos    : ro;
-sub length : ro;
-
-sub start ($self) { $self->{pos} - $self->{length} }
+sub start : ro;
 
 1;
 
