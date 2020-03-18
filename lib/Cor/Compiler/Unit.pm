291094d1c6f78f4ec86902ed36edeac826c2dc6d
Stevan Little <stevan.little@gmail.com>
2020-03-18 20:31:05 +0100

    add a name method to the Compiler::Unit role

--- lib/Cor/Compiler/Unit.pm
+++ lib/Cor/Compiler/Unit.pm
@@ -2,10 +2,13 @@ package Cor::Compiler::Unit;
 
 use v5.24;
 use warnings;
+use experimental qw[ signatures ];
 
 use slots (
     ast => sub {},
 );
+
+sub name ($self) { $self->{ast}->name }
 
 sub generate_source;
 
