e4af4bf5bea60c56716cc1c2ff0c0c86aac9f07c
Stevan Little <stevan.little@gmail.com>
2020-04-01 20:23:19 +0200

    Keep track of what we have build already

--- lib/Cor.pm
+++ lib/Cor.pm
@@ -14,10 +14,23 @@ use Cor::Compiler;
 
 use constant DEBUG => $ENV{COR_DEBUG} // 0;
 
+our %COR_INC;
+
 sub build ($resource, %opts) {
 
-    my $full_package_path = find_module_path_in_INC( $resource )
-        or die "Could not find [$resource] in \@INC paths";
+    my ($package_dir, $package_path) = find_module_path_in_INC( $resource );
+
+    die "Could not find [$resource] in \@INC paths"
+        unless defined $package_dir;
+
+    if ( exists $COR_INC{ $package_path } ) {
+        warn "Skipping [$resource]($package_path) it was already built\n" if DEBUG;
+        return;
+    }
+
+    warn "Building [$resource]($package_path) in ($package_dir)\n" if DEBUG;
+
+    my $full_package_path = File::Spec->catfile( $package_dir, $package_path );
 
     my $original = read_source_file( $full_package_path );
     my $doc      = Cor::Parser::parse( $original );
@@ -35,18 +48,18 @@ sub build ($resource, %opts) {
     }
 
     my $compiled = $compiler->compile;
-    push @built => write_pmc_file( $full_package_path, $compiled );
 
-    return @built;
+    my $pmc_file_path = write_pmc_file( $full_package_path, $compiled );
+
+    $COR_INC{ $package_path } = $pmc_file_path;
+
+    return @built, $pmc_file_path;
 }
 
 sub find_module_path_in_INC ($resource) {
     my @inc = @INC;
 
     my $package_path;
-
-    # if it is an absolute file path, just return it
-    return $resource if File::Spec->file_name_is_absolute( $resource );
 
     if ( Module::Runtime::is_module_name( $resource ) ) {
         $package_path = Module::Runtime::module_notional_filename( $resource );
@@ -55,20 +68,20 @@ sub find_module_path_in_INC ($resource) {
         $package_path = $resource;
     }
 
-    my ($inc, $full_package_path);
+    my ($inc, $package_dir);
     while ( $inc = shift @inc ) {
         next if ref $inc; # skip them for now ...
 
         # build the path
-        $full_package_path = File::Spec->catfile( $inc, $package_path );
+        $package_dir = $inc;
         # jump out of loop if we found it
-        last if -f $full_package_path;
+        last if -f File::Spec->catfile( $inc, $package_path );
         # otherwise undef the variable
         # and continue checking
-        undef $full_package_path;
+        undef $package_dir;
     }
 
-    return $full_package_path;
+    return ($package_dir, $package_path);
 }
 
 sub read_source_file ($full_package_path) {
