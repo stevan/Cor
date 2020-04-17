37c61f57f8237269163905bab7f0508781e32fa4
Stevan Little <stevan.little@gmail.com>
2020-04-17 02:50:21 +0200

    the constructor does not make sense as an AST concept, it is not defined in the file, so therefore it makes more sense to be a compiler thing

--- lib/Cor/Parser/AST/Class.pm
+++ lib/Cor/Parser/AST/Class.pm
@@ -6,17 +6,13 @@ use warnings;
 use experimental qw[ signatures postderef ];
 use decorators   qw[ :accessors ];
 
-use Cor::Parser::AST::Constructor;
-
 use parent 'Cor::Parser::AST::Role';
 
 use slots (
     superclasses => sub { [] },
-    constructor  => sub { Cor::Parser::AST::Constructor->new },
 );
 
 sub superclasses : ro;
-sub constructor  : ro;
 
 sub add_superclass ($self, $superclass) {
     # TODO - test that $superclass is a Builder::Reference
@@ -27,16 +23,6 @@ sub has_superclasses ($self) { !! $self->{superclasses}->@* }
 
 sub has_superclass ($self, $name) { !! scalar grep $_->name eq $name, $self->{methods}->@* }
 
-sub add_slot ($self, $slot) {
-    $self->next::method( $slot );
-
-    # XXX: make sure that $slot has a name
-
-    $self->{constructor}->add_parameter_mapping(
-        $slot->identifier,
-        $slot->name
-    );
-}
 
 1;
 
